543ee0c3c47fab5ecae55ceba2f202e4c429291e
Gabriel_Marinho <biel32771@gmail.com>
2020-08-28 19:07:28 -0300

    Adicionado opção de salvar ao projeto manipulador de cores

--- Projetos/Manipulando_cores/js/js.js
+++ Projetos/Manipulando_cores/js/js.js
@@ -2,6 +2,16 @@ window.addEventListener('load', start);
 let red = document.querySelector('#red');
 let green = document.querySelector('#green');
 let blue = document.querySelector('#blue');
+const blockColor = document.querySelector('#cores-salvas');
+let acumulator = '';
+let quadro = document.querySelector('#quadro');
+let span = document.querySelector('#mostrar');
+const bodyHTML = document.querySelector('#body');
+let spanColor = 'rgb(0,0,0)';
+
+const spanRed = document.querySelector('#vRed');
+const spanGreen = document.querySelector('#vGreen');
+const spanBlue = document.querySelector('#vBlue');
 
 function start() {
   // Recuperando dado do input
@@ -9,28 +19,24 @@ function start() {
   red.addEventListener('change', vermelho);
   green.addEventListener('change', verde);
   blue.addEventListener('change', azul);
+  const star = document.querySelector('#star');
+
+  star.addEventListener('click', () => {
+    insertColorOptions();
+  });
 }
 
 function vermelho(event) {
-  //salvando valor do input
-  //var valorRed = event.target.value;
-
-  //Adcionando o valor ao input disable
-  const spanRed = document.querySelector('#vRed');
   spanRed.innerHTML = event.target.value;
   adicionaCor();
 }
 
 function verde(event) {
-  //var valorGreen = event.target.value;
-  const spanGreen = document.querySelector('#vGreen');
   spanGreen.innerHTML = event.target.value;
   adicionaCor();
 }
 
 function azul() {
-  //var valorBlue = event.target.value;
-  const spanBlue = document.querySelector('#vBlue');
   spanBlue.innerHTML = event.target.value;
   adicionaCor();
 }
@@ -39,13 +45,32 @@ function adicionaCor() {
   red = document.querySelector('#red').value;
   green = document.querySelector('#green').value;
   blue = document.querySelector('#blue').value;
-  let quadro = document.querySelector('#quadro');
-  let span = document.querySelector('#mostrar');
-  const bodyHTML = document.querySelector('#body');
 
-  span.textContent = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-
+  span.textContent = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  const rgb = `rgb( ${red} , ${green} , ${blue} )`;
   //Adicionando cor de fundo
-  quadro.style.backgroundColor = `rgb(${red},${green},${blue})`;
-  bodyHTML.style.backgroundColor = `rgb(${red},${green},${blue})`;
+  quadro.style.backgroundColor = rgb;
+  bodyHTML.style.backgroundColor = rgb;
+  spanColor = rgb;
 }
+
+const insertColorOptions = () => {
+  acumulator += `<div class="block-color" onClick="handleFavorite(
+    '${spanColor}'
+  )" style="background-color:${spanColor} "> </div>`;
+
+  blockColor.innerHTML = acumulator;
+};
+
+const handleFavorite = (rgb) => {
+  // Retirando os espaços
+  span.innerHTML = rgb.replace(/\s/g, '');
+  const arr = rgb.split(' ');
+
+  spanBlue.innerHTML = arr[1];
+  spanGreen.innerHTML = arr[3];
+  spanRed.innerHTML = arr[5];
+
+  quadro.style.backgroundColor = rgb;
+  bodyHTML.style.backgroundColor = rgb;
+};
