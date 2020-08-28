d69c60da9d47db92cc02dbfa97b908924c6147de
Gabriel_Marinho <biel32771@gmail.com>
2020-08-28 14:36:57 -0300

    Ajustes no app  pokedex

--- Projetos/pokedex-master/app.js
+++ Projetos/pokedex-master/app.js
@@ -1,34 +1,44 @@
-window.addEventListener('load', () => {
-  const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const apiPokemon = [];
 
-  const generetePokemonPromises = () =>
-    Array(150)
-      .fill()
-      .map((_, index) =>
-        fetch(getPokemonUrl(index + 1)).then((response) => response.json())
-      );
-
-  const generateHTML = (pokemons) =>
-    pokemons.reduce((accumulator, { name, id, types }) => {
-      const ElementTypes = types.map((typeInfo) => typeInfo.type.name);
-
-      accumulator += `
-          <li class="card ${ElementTypes[0]}">
-          <img class="card-image" alt="${name}"
-             src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
-          <h2 class="card-title">${id}. ${name}</h2>
-          <p class="card-subtitle">${ElementTypes.join(' | ')}</p>
-          </li>
-          `;
-      return accumulator;
-    }, '');
-
-  const insertPokemonIntoPage = (pokemons) => {
-    const ul = document.querySelector('[data-js="pokedex"]');
-    ul.innerHTML = pokemons;
-  };
-
-  const pokemonPromises = generetePokemonPromises();
-
-  Promise.all(pokemonPromises).then(generateHTML).then(insertPokemonIntoPage);
+window.addEventListener('load', async () => {
+  await getApiPokemon();
+  await mapPokemom(apiPokemon);
 });
+
+const mapPokemom = async (allApiPokemom) => {
+  const pokedexHTML = document.querySelector('#pokedex');
+
+  let accumulator = '';
+
+  allApiPokemom.forEach(({ name, id, types }) => {
+    const type = types[0].type.name;
+
+    let allType = '';
+
+    if (types[1] != undefined) {
+      allType = `${type} | ${types[1].type.name}`;
+    } else {
+      allType = type;
+    }
+
+    accumulator += `
+           <a data-toggle="modal"
+        data-target="#pokemon-modal-${id}"> <li class="card ${type}" >
+            <img class="card-image" alt="${name}"
+               src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
+            <h2 class="card-title">${id}. ${name}</h2>
+            <p class="card-subtitle">${allType}</p>
+            </li>
+            </a>
+            `;
+  });
+
+  pokedexHTML.innerHTML = accumulator;
+};
+
+const getApiPokemon = async () => {
+  for (let i = 1; i <= 152; i++) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    apiPokemon.push(await res.json());
+  }
+};
