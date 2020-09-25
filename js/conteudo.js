d9f31109d38c44e3bc308b0f9720b0bc4768fbd0
Gabriel_Marinho <biel32771@gmail.com>
2020-09-25 14:40:46 -0300

    Realocando arrays para arqquivo conteudo

---
+++ js/conteudo.js
@@ -0,0 +1,112 @@
+const menu = [
+  {
+    id: 'app-mata-mosquito',
+    titulo: 'APP Mata Mosquito',
+    link: './Projetos/App_Mata_Mosquito/inicio_jogo.html',
+    img: './Imagens/projetos-img/App-mata-mosquito.png',
+  },
+  {
+    id: 'pesquisa-de-usuarios',
+    titulo: 'Pesquisa de Usuários ',
+    link: './Projetos/Pesquisa-de-usuarios/index.html',
+    img: './Imagens/projetos-img/Pesquisa_de_usuarios.png',
+  },
+  {
+    id: 'pokedex',
+    titulo: 'Pokedex',
+    link: './Projetos/pokedex-master/index.html',
+    img: './Imagens/projetos-img/Pokedez.png',
+  },
+  {
+    id: 'controle-financeiro-pessoal',
+    titulo: 'Controle Financeiro Pessoal',
+    link: 'https://gabriel-controlhe-financeiro.herokuapp.com/',
+    img: './Imagens/projetos-img/Controlhe-finaceiro.png',
+  },
+  {
+    id: 'radio-podcast',
+    titulo: 'Rádio Podcast',
+    link: './Projetos/radio-podcast-pronto/index.html',
+    img: './Imagens/projetos-img/Radio_Podcast.png',
+  },
+  {
+    id: 'manipulador-de-cores',
+    titulo: 'Manipulador de Cores',
+    link: './Projetos/Manipulando_cores/index.html',
+    img: './Imagens/projetos-img/Manipulador_de_cores.png',
+  },
+  {
+    id: 'react-countries',
+    titulo: 'React Countries',
+    link: 'https://gabriel-react-countries.herokuapp.com/',
+    img: './Imagens/projetos-img/React-Countries.png',
+  },
+  {
+    id: 'react-salary',
+    titulo: 'React Salary',
+    link: 'https://grabriel-salary.herokuapp.com/',
+    img: './Imagens/projetos-img/React-salary.png',
+  },
+  {
+    id: 'react-juros-compostosy',
+    titulo: 'React - Juros Compostos',
+    link: 'https://react-juros-compostos.herokuapp.com/',
+    img: './Imagens/projetos-img/Rect_juros_compostos.png',
+  },
+  {
+    id: 'tabela-brasileirão',
+    titulo: 'Tabela do Brasileirão',
+    link: './Projetos/Brasileirão/index.html',
+    img: './Imagens/projetos-img/Tabela-Brasileirao.png',
+  },
+];
+
+const menuTrabalhos = [
+  {
+    id: 'awp-brasil',
+    titulo: 'Sistema AWP Brasil',
+    link: 'https://atcawpbrasil.com.br/',
+    subTitulo: 'AWP Brasil',
+    img: './Imagens/projetos-img/Assistencia_AWP.png',
+  },
+  {
+    id: 'valorum',
+    titulo: 'Valorum',
+    link: 'https://valorum.com.br/',
+    subTitulo: 'Mix7',
+    img: './Imagens/projetos-img/Valorum.png',
+  },
+  {
+    id: 'folha-dedetizadora',
+    titulo: 'Folha Detdetizadora',
+    subTitulo: 'Mix7',
+    link: 'https://folhadedetizadora.com.br/',
+    img: './Imagens/projetos-img/folha-dedetizadora.png',
+  },
+  {
+    id: 'aria-pilates',
+    titulo: 'Aria Pilates',
+    subTitulo: 'Mix7',
+    link: 'https://ariapilates.com.br/',
+    img: './Imagens/projetos-img/Aria-pilates.png',
+  },
+];
+
+const redesSociais = [
+  {
+    id: 'email',
+    link: 'mailto:gabrielmarinhodossantoscd@gmail.com',
+    img:
+      './Imagens/email-open-envelope-in-a-rounded-square_icon-icons.com_70497.png',
+  },
+  {
+    id: 'github',
+    link: 'https://github.com/gabrielMarinhocd/',
+    img: './Imagens/github-logo_icon-icons.com_73546.png',
+  },
+  {
+    id: 'email',
+    link: 'https://www.linkedin.com/in/gabriel-marinho-5094bb132',
+    img: './Imagens/linkedin-sign_icon-icons.com_73508.png',
+  },
+];
--- js/js.js
+++ js/js.js
@@ -47,115 +47,6 @@ function verificaHora() {
   document.querySelector('#boas-vindas').innerHTML = time;
 }
 
-const menu = [
-  {
-    id: 'app-mata-mosquito',
-    titulo: 'APP Mata Mosquito',
-    link: './Projetos/App_Mata_Mosquito/inicio_jogo.html',
-    img: './Imagens/projetos-img/App-mata-mosquito.png',
-  },
-  {
-    id: 'pesquisa-de-usuarios',
-    titulo: 'Pesquisa de Usuários ',
-    link: './Projetos/Pesquisa-de-usuarios/index.html',
-    img: './Imagens/projetos-img/Pesquisa_de_usuarios.png',
-  },
-  {
-    id: 'pokedex',
-    titulo: 'Pokedex',
-    link: './Projetos/pokedex-master/index.html',
-    img: './Imagens/projetos-img/Pokedez.png',
-  },
-  {
-    id: 'controle-financeiro-pessoal',
-    titulo: 'Controle Financeiro Pessoal',
-    link: 'https://gabriel-controlhe-financeiro.herokuapp.com/',
-    img: './Imagens/projetos-img/Controlhe-finaceiro.png',
-  },
-  {
-    id: 'radio-podcast',
-    titulo: 'Rádio Podcast',
-    link: './Projetos/radio-podcast-pronto/index.html',
-    img: './Imagens/projetos-img/Radio_Podcast.png',
-  },
-  {
-    id: 'manipulador-de-cores',
-    titulo: 'Manipulador de Cores',
-    link: './Projetos/Manipulando_cores/index.html',
-    img: './Imagens/projetos-img/Manipulador_de_cores.png',
-  },
-  {
-    id: 'react-countries',
-    titulo: 'React Countries',
-    link: 'https://gabriel-react-countries.herokuapp.com/',
-    img: './Imagens/projetos-img/React-Countries.png',
-  },
-  {
-    id: 'react-salary',
-    titulo: 'React Salary',
-    link: 'https://grabriel-salary.herokuapp.com/',
-    img: './Imagens/projetos-img/React-salary.png',
-  },
-  {
-    id: 'react-juros-compostosy',
-    titulo: 'React - Juros Compostos',
-    link: 'https://react-juros-compostos.herokuapp.com/',
-    img: './Imagens/projetos-img/Rect_juros_compostos.png',
-  },
-  {
-    id: 'tabela-brasileirão',
-    titulo: 'Tabela do Brasileirão',
-    link: './Projetos/Brasileirão/index.html',
-    img: './Imagens/projetos-img/Tabela-Brasileirao.png',
-  },
-];
-
-const menuTrabalhos = [
-  {
-    id: 'awp-brasil',
-    titulo: 'Projeto AWP Brasil',
-    link: 'https://atcawpbrasil.com.br/',
-    img: './Imagens/projetos-img/Assistencia_AWP.png',
-  },
-  {
-    id: 'valorum',
-    titulo: 'Valorum',
-    link: 'https://valorum.com.br/',
-    img: './Imagens/projetos-img/Valorum.png',
-  },
-  {
-    id: 'folha-dedetizadora',
-    titulo: 'Folha Detdetizadora',
-    link: 'https://folhadedetizadora.com.br/',
-    img: './Imagens/projetos-img/folha-dedetizadora.png',
-  },
-  {
-    id: 'aria-pilates',
-    titulo: 'Aria Pilates',
-    link: 'https://ariapilates.com.br/',
-    img: './Imagens/projetos-img/Aria-pilates.png',
-  },
-];
-
-const redesSociais = [
-  {
-    id: 'email',
-    link: 'mailto:gabrielmarinhodossantoscd@gmail.com',
-    img:
-      './Imagens/email-open-envelope-in-a-rounded-square_icon-icons.com_70497.png',
-  },
-  {
-    id: 'github',
-    link: 'https://github.com/gabrielMarinhocd/',
-    img: './Imagens/github-logo_icon-icons.com_73546.png',
-  },
-  {
-    id: 'email',
-    link: 'https://www.linkedin.com/in/gabriel-marinho-5094bb132',
-    img: './Imagens/linkedin-sign_icon-icons.com_73508.png',
-  },
-];
-
 const insertMenuDropdown = () => {
   const idDropdown = document.querySelector('#dropdown1');
   const navMobile = document.querySelector('#dropdown-mobile');
@@ -210,13 +101,15 @@ const insertCaroselTrabalhos = () => {
   const caroselHTML = document.querySelector('#carouselTrabalhos');
 
   let gradeHTML = '';
-  menuTrabalhos.forEach(({ id, titulo, link, img }) => {
+  menuTrabalhos.forEach(({ id, titulo, link, img, subTitulo }) => {
     gradeHTML += `  <a id="link-${id}" class="carousel-item modal-trigger"  href="${link}" target="_blank"
               ><img id="img-${id}"
                 class="img-carosel"
                 src="${img}"
               />
               <span id="span-${id}" >${titulo}</span>
+              <br/>
+              <span id="span-sub${id}" >${subTitulo}</span>
             </a>`;
   });
 
